97da4ca2aa64e4d04e078afe5ca4b75c444ea2f2
Gdominguezborines <Gdominguezborines@danielcastelao.org>
2023-01-23 19:06:07 +0100

    version 1

--- login/funciones_login.php
+++ login/funciones_login.php
@@ -1,97 +1,74 @@
 <?php
-
 //funcion que recoge loggin
-function html($err = " ", $num = " ") {
+function html($err = " ",$num=" " ) {
     ?>
 
-    <html lang="ES">
+    <html>
         <head>
             <title>title</title>
         </head>
         <body>
             <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" >
                 <h2>Login:</h2>
-                <label for="c1" class="form-label">Mail:</label><br>     
-                <input type="email" name="mail" class="form-control" id="c1"  <?php
-                if (!empty($_POST['mail'])) {
-                    echo " value='" . $_POST['mail'] . "'";
-                }
-                ?>   ><br>
-                <label for="c2" class="form-label">Contraseña:</label><br>
-                <input type="password" name="pass" class="form-control" id="c2"  ><br><br>
+                Mail:<br>     
+                <input type="email" name="mail"   <?php if(!empty($_POST['mail'])){ echo " value='" . $_POST['mail'] . "'";} ?>   ><br>
+                Contraseña:<br>
+                <input type="password" name="pass"   ><br><br>
                 <input type="submit" name="login">
                 <br> <?php echo $err; ?><br> 
-                <?php echo $num; ?> <br>
-
-                <p> <a href='recuperarContra.php 'style="color:red">Recuperar</a><b> tu contraseña!!!  </b></p> 
-                <p> <b>No estas dado de alta!!!  </b><a href='registro.php 'style="color:blue">Registrate</a> para tener mas ventajas</p>
+                     <?php echo $num; ?> <br>
+                
+                     <p> <a href='recuperarContra.php 'style="color:red">Recuperar</a><b> tu contraseña!!!  </b></p> 
+                     <p> <b>No estas dado de alta!!!  </b><a href='registro.php 'style="color:blue">Registrate</a> para tener mas ventajas</p>
             </form>
-
-
-
+       
+           
+        
         </body>
     </html>
 
     <?php
 }
-
 //funcion que recoge datos de registro
-function htmlRegistro($necesarios = "") {
+function htmlRegistro($necesarios="") {
     ?>
 
-    <html lang="ES">
+    <html>
         <head>
             <title>title</title>
         </head>
         <body>
             <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" >
                 <h2>Registro:</h2>
-                <label for="c1" class="form-label">Nombre:</label><br>
-                <input type="text" name="nombre" class="form-control" id="c1" <?php
-                if (!empty($_POST['nombre'])) {
-                    echo " value='" . $_POST['nombre'] . "'";
-                }
-                ?> ><br>
-                <label for="c2" class="form-label">Primer Apellido:</label><br>
-                <input type="text" name="apellido1" class="form-control" id="c2" <?php
-                if (!empty($_POST['apellido1'])) {
-                    echo " value='" . $_POST['apellido1'] . "'";
-                }
-                ?> ><br>
-                <label for="c3" class="form-label">Segundo Apellido:</label><br>
-                <input type="text" name="apellido2" class="form-control" id="c3" <?php
-                if (!empty($_POST['apellido2'])) {
-                    echo " value='" . $_POST['apellido2'] . "'";
-                }
-                ?> ><br>
-                <label for="c4" class="form-label">Mail:</label><br>     
-                <input type="email" name="mail" class="form-control" id="c4"  <?php
-                if (!empty($_POST['mail'])) {
-                    echo " value='" . $_POST['mail'] . "'";
-                }
-                ?>   ><br>
-                <label for="c5" class="form-label">Telefono:</label><br>
-                <input type="text" name="telefono" class="form-control" id="c5" <?php
-                if (!empty($_POST['telefono'])) {
-                    echo " value='" . $_POST['telefono'] . "'";
-                }
-                ?>   ><br>
-                <br> 
-
-
+                Nombre:<br>
+                <input type="text" name="nombre" <?php if(!empty($_POST['nombre'])){ echo " value='" . $_POST['nombre'] . "'";} ?> ><br>
+                Primer Apellido:<br>
+                <input type="text" name="apellido1" <?php if(!empty($_POST['apellido1'])){ echo " value='" . $_POST['apellido1'] . "'";} ?> ><br>
+                Segundo Apellido:<br>
+                <input type="text" name="apellido2" <?php if(!empty($_POST['apellido2'])){ echo " value='" . $_POST['apellido2'] . "'";} ?> ><br>
+                Mail:<br>     
+                <input type="email" name="mail"   <?php if(!empty($_POST['mail'])){ echo " value='" . $_POST['mail'] . "'";} ?>   ><br>
+                Telefono:<br>
+                <input type="text" name="telefono" <?php if(!empty($_POST['telefono'])){ echo " value='" . $_POST['telefono'] . "'";} ?>   ><br>
+                Contraseña:<br>
+                <input type="password" name="pass"   ><br>
+                Repite contraseña:<br>
+                <input type="password" name="pass2"   ><br><br>
                 <input type="submit" name="registro">
+           
                 <?php
+                
                 if (!empty($_POST['registro']) && $necesarios !== true) {
-                    //Enseña los campos que faltan al usuario
-                    $necesarios = str_replace('apellido1', 'primer apellido', $necesarios);
-                    // $necesarios = str_replace('apellido2', 'segundo apellido', $necesarios);
-                    $necesarios = str_replace('password', 'contraseña', $necesarios);
-                    // $necesarios = str_replace('password2', 'confirmación de la contraseña',$necesarios);
-                    $necesarios = str_replace('email', 'correo', $necesarios);
-                    echo "<br><br><b style=color:red>Faltan campos obligatorios:</b> <br>$necesarios";
-                }
-                ?>
-
+                  //Enseña los campos que faltan al usuario
+                            $necesarios = str_replace('apellido1', 'primer apellido', $necesarios );        
+                           // $necesarios = str_replace('apellido2', 'segundo apellido', $necesarios);
+                            $necesarios = str_replace('password', 'contraseña', $necesarios);
+                           // $necesarios = str_replace('password2', 'confirmación de la contraseña',$necesarios);
+                            $necesarios = str_replace('email', 'correo', $necesarios);
+                         echo "<br><br><b style=color:red>Faltan campos obligatorios:</b> <br>$necesarios";
+                        }
+                    ?>
+                
             </form>
 
         </body>
@@ -100,53 +77,20 @@ function htmlRegistro($necesarios = "") {
     <?php
 }
 
-function tokenMail() {
-    ?> 
-    <!DOCTYPE html>
-    <html lang="ES">
-        <head>
-        </head>
-        <body>
-            <div  > 
-                <form   action="<?php htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-                    <h2>Comprobar Mail</h2>
-                    <div  >
-                        <label for="c" class="form-label">Codigo Mail:</label><br>
-                        <input type="password" class="form-control" id="c" name="token"><br><br>
-                        <input type="hidden" name="testigo" >
-                    </div>
-                    <input type="submit" name="validar" value="Activar Cuenta" ></input>
-                </form> 
-            </div>
-
-        </body>
-    </html>
-    <?php
+function comprobartoken( ) {
+ ?>
+  <div >
+                <form action="comprobarToken.php" method="POST">
+                    <input type="hidden" name="pass" <?php if(!empty($_POST['pass'])){ echo " value='" . $_POST['pass'] . "'";} ?> ><br>
+                    <input type="hidden" name="mail" <?php if(!empty($_POST['mail'])){ echo " value='" . $_POST['mail'] . "'";} ?>>
+                    <input type="submit" value="Verificar Cuenta">
+                </form>
+    </div>
+<?php
 }
 
-function contraMail() {
-    ?>
-    <html lang="ES">
-        <head>
-        </head>
-        <body>
-            <div  >
-                <form   action="guardarContraseña.php" method="POST">
-               
-                    <label for="c2" class="form-label">Escriba su contraseña:</label><br>
-                    <label for="c3" class="form-label">Contraseña:</label><br>
-                    <input type="password" name="pass" class="form-control" id="c3"  ><br>
-                    <label for="c4" class="form-label">Repite contraseña:</label><br>
-                    <input type="password" name="pass2" class="form-control" id="c4"  ><br><br> 
-
-                    <input type="submit" name="contraMail" value="PULSAR PARA VERIFICAR"></input>
-                </form> 
-            </div>
-        </body>
-    </html>
-    <?php
-}
-
+ 
+ 
 function campos(array $campos, $requeridos) {
     $noCubiertos = [];
     foreach ($campos as $campo) {
@@ -161,14 +105,20 @@ function campos(array $campos, $requeridos) {
     }
     return $respuesta;
 }
+ 
+ 
 
-if (!empty($_POST['send_register']) && $required !== true) {
-    //Para que nos muestre en castellano los campos que faltan
-    $required = str_replace('name', 'nombre', $required);
-    $required = str_replace('password_confirm', 'confirmación de la password', $required);
-    $required = str_replace('birthday', 'fecha de nacimiento', $required);
-    echo "Faltan campos obligatorios: $required";
-}
+
+
+ 
+  if (!empty($_POST['send_register']) && $required !== true) {
+           //Para que nos muestre en castellano los campos que faltan
+                $required = str_replace('name', 'nombre', $required);
+                $required = str_replace('password_confirm', 'confirmación de la password', $required);
+                $required = str_replace('birthday', 'fecha de nacimiento', $required);
+                echo "Faltan campos obligatorios: $required";
+            }
+
 
 function texto($texto) {
     /*
@@ -179,40 +129,30 @@ function texto($texto) {
 }
 
 function contraseña($pass, $pass2) {
-
+       
     if ($pass == $pass2) {
         return true;
-    } else {
-        return false;
-    }
+    } else{
+    return false;}
+    
 }
 
 function correo($mail) {
 
     return filter_var($mail, FILTER_SANITIZE_EMAIL);
 }
-
 /*
-  function cookis($var1, $var2 = " ", $var3 = " ") {
+ function cookis($var1, $var2 = " ", $var3 = " ") {
+             
+            if (empty($var3)) {
+                $cook1 = setcookie($var1, $var2);
+            } else { 
+                $cook1 = setcookie($var1, $var2, time() + $var3);
+                 
+            }
 
-  if (empty($var3)) {
-  $cook1 = setcookie($var1, $var2);
-  } else {
-  $cook1 = setcookie($var1, $var2, time() + $var3);
-
-  }
-
-  if ($cook1) {
-  echo "cookie creada<br>";
-  }
-  }
- */
-
-function hora() {
-    $a = getdate(time());
-    return $a["hours"] . ":" . $a["minutes"] . ":" . $a["seconds"];
-}
-
-function fechaActual() {
-    return $fecha_actual = date("Y-m-d ");
-}
+            if ($cook1) {
+                echo "cookie creada<br>";
+            }
+        }
+*/
--- login/registro.php
+++ login/registro.php
@@ -1,100 +1,38 @@
 <?php
-require_once "../conexion/conexion.php";
-include "funciones_login.php";
-include"mail_reset.php";
 session_start();
- 
- 
+include "funciones_login.php";
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST['nombre'];
     $apellido1 = $_POST['apellido1'];
     $apellido2 = $_POST['apellido2'];
     $telefono = $_POST['telefono'];
     $mail = $_POST['mail'];
-    $rol="registrado";
-    $fecha=fechaActual();
-   // $contra = $_POST['pass'];
-   // $contra2 = $_POST['pass2'];
-    //, "password" => $contra, "password2" => $contra2  , 'password', 'password2'
+    $contra = $_POST['pass'];
+    $contra2 = $_POST['pass2'];
 
-    $campos = array("nombre" => $nombre, "apellido1" => $apellido1, "telefono" => $telefono, "email" => $mail); //mail base de datos y contraseña
+    $campos = array("nombre" => $nombre, "apellido1" => $apellido1, "telefono" => $telefono, "email" => $mail, "password" => $contra, "password2" => $contra2); //mail base de datos y contraseña
 
-    $necesarios = campos(['nombre', 'apellido1', 'telefono', 'email'], $campos);
+    $necesarios = campos(['nombre', 'apellido1', 'telefono', 'email', 'password', 'password2'], $campos);
 
     if (!isset($_POST['registro']) || (isset($_POST['registro']) && !is_string($necesarios))) {
 
-        $tiempo = 6000;
-        if (isset($_COOKIE['sessionTemporal'])) {
-        setcookie('sessionTemporal', "a", time() - $tiempo);
-        }
-        
-            //datos sql
-        // aqui se le indica que se le envio un codigo de comprovacion al mail
-        $token = intval(rand(5000, 200000) * 25 / 4 + 3);
-        //$token=123456;
-        $token2 = password_hash($token, PASSWORD_DEFAULT);
-        mail($mail,$nombre,$token);
-        $datos = array($nombre, $apellido1, $token2, $telefono, $mail, $apellido2);
-            
-            $a = implode(",", $datos);
+        static $cont = 0;
+        if (contraseña($contra, $contra2) && correo($mail) == "pepe@gmail.com") { //datos sql
+            // aqui se le indica que se le envio un codigo de comprovacion al mail
+            //cookis($nombre, $valor, $tiempo);
+            htmlRegistro($necesarios);
+           comprobartoken( );
  
-                setcookie('sessionTemporal', $a, time() + $tiempo);
-        
 
-                
-        
-        try {
-            $conexion = conexion();
-
-            $sql = "INSERT INTO usuario (nombre,apellido1 ,apellido2,contraseña,correo,num_telef, rol,fecha) VALUES (:nombre,:apellido1,:apellido2,:contrasena,:correo,:num_telef,:rol,:fecha)";
-
-            $stmt = $conexion->prepare($sql);
-
-            $stmt->bindParam(':nombre', $nombre, PDO::PARAM_STR, 25);
-            $stmt->bindParam(':apellido1', $apellido1, PDO::PARAM_STR, 25);
-            $stmt->bindParam(':apellido2', $apellido2, PDO::PARAM_STR, 25);
-            $stmt->bindParam(':contrasena', $token2, PDO::PARAM_STR);
-            $stmt->bindParam(':correo', $mail, PDO::PARAM_STR, 50);
-            $stmt->bindParam(':num_telef', $telefono, PDO::PARAM_STR);
-            $stmt->bindParam(':rol',$rol , PDO::PARAM_STR);
-            $stmt->bindParam(':fecha', $fecha, PDO::PARAM_STR);
-
-            $stmt->execute();
-            
-           if($stmt ==true ){
-                
-           }else{
-               echo "fallo";
-           }
-        
- 
-            unset($conexion);
-        } catch (PDOException $e) {
-            echo 'Accion no realizada porque:<br>';
-             if($e->getCode()==23000){
-                echo "<br>El correo introducido <b>ya existe</b> en la base de datos.<br><br> ";
-            }
-            die("ERROR: " . $e->getMessage() . "<br>" . $e->getCode());
-           
-            
-        }
-                
-                 
-                if($necesarios== true){
-                   header("Location: comprobarToken.php");
-                
-            }else{
-                 htmlRegistro($necesarios);            
-            }
-                
-         
-    } else {
-
-        htmlRegistro($necesarios);
-    }
 } else {
-    htmlRegistro();
+
 }
+} else {
 
+htmlRegistro($necesarios);
+} 
 
- 
+}else{
+htmlRegistro();
+}

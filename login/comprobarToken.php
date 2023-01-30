d55ed7f93365c0c00993e0e52c8c892249a8409e
Luis Guillermo Andre <guille1insua@gmail.com>
2023-01-30 10:22:48 +0100

    Back, Front, BD, draw

--- login/comprobarToken.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
- session_start();
- echo $mail = $_POST['mail']."<br>";
- echo $contra = $_POST['pass']."<br>";
- echo "Registro correcto   <br> <b>Rebise su Email para finalizar el proceso</b><br><br> No cierre el navegador gracias";
-          

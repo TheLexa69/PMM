416cd2a26cc9202f688d7b74aad39c5e4615b7bb
Gabriel <Gabrieldb050890@gmail.com>
2023-03-01 08:27:54 +0100

    cambios nuria

--- backend/clases/Carrito.php
+++ backend/clases/Carrito.php
@@ -105,25 +105,27 @@ class Carrito extends Conexion {
         $result = $stmt->fetch();
 
         //Código para visualizar el carro
-        $url = DIRECTORY_SEPARATOR .'proyecto'.DIRECTORY_SEPARATOR .'backend'. DIRECTORY_SEPARATOR . 'cart'. DIRECTORY_SEPARATOR.'eliminar_carrito.php?cod='. $result['id_comida'];
+        $url = DIRECTORY_SEPARATOR .'proyecto'.DIRECTORY_SEPARATOR .'backend'. DIRECTORY_SEPARATOR . 'cart'. DIRECTORY_SEPARATOR.'eliminar_carrito.php?cod='. $result['id_comida'] . '&red=1';
         $html_code = '<div class="row align-items-center border-bottom pt-2 pb-2">
-                    <div class="col-3">'.$result['nombre'].'</div>
-                    <div class="col-3">'.$result['precio'].'</div>
-                    <div class="col-3">'.$cantidad.'</div>
-                    <div class="col-3 d-flex justify-content-center align-items-center">
-                        <a href="'.$url.'">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-trash" viewBox="0 0 16 16">
-                                <path
-                                    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-                                <path fill-rule="evenodd"
-                                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
-                            </svg>
-                        </a>
-                    </div>
-                </div>';
+                <div class="col-3">'.$result['nombre'].'</div>
+                <div class="col-3">'.$result['precio'].'</div>
+                <div class="col-3">
+                <input type="number" name="cantidad" size="5" value="'. $cantidad . '" min="1" max="10" onchange="updateCantidad(\'' . $id_comida . '\', this.value)"></div>
+                <div class="col-3 d-flex justify-content-center align-items-center">
+                    <a href="'.$url.'">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-trash" viewBox="0 0 16 16">
+                            <path
+                                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                            <path fill-rule="evenodd"
+                                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                        </svg>
+                    </a>
+                </div>
+            </div>';
 
 return $html_code;
+
     }
     
     /*Añadir productos al carro*/
@@ -164,7 +166,6 @@ return $html_code;
     }*/
 
     public function getTotalPrice($carrito) {
-        $carrito = unserialize($carrito, []);
         $precio = 0;
         // Obtener todos los productos en el carrito
         foreach($carrito as $cod => $cant) {
@@ -172,10 +173,12 @@ return $html_code;
             $stmt->bindParam(1, $cod, PDO::PARAM_INT);
             $stmt->execute();
             $productos = $stmt->fetch();
-            $precio += (int)$productos['precio'] * (int)$cant;
+            if ($productos) {
+                $precio += (double)$productos['precio'] * (double)$cant;
+            }
         }
     
-        return $precio;
+        return number_format($precio,2) . "€";
     }
     
 

f4147b7840ca806d3d7d0b40b3fa811d5407ccb7
nuriaabg <nuriabucetagarcia@gmail.com>
2023-06-10 19:20:16 +0200

    pdf

--- backend/cart/procesar_pedido.php
+++ backend/cart/procesar_pedido.php
@@ -1,13 +1,14 @@
 <?php
-ob_clean();
 require "../sesiones/sesiones.php";
 //session_start();
 comprobar_sesion();
-require(dirname(__DIR__,2) .DIRECTORY_SEPARATOR ."frontend". DIRECTORY_SEPARATOR . "php". DIRECTORY_SEPARATOR . "nav.php"); 
+require(dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . "frontend" . DIRECTORY_SEPARATOR . "php" . DIRECTORY_SEPARATOR . "nav.php");
+
 use \clases\Pedido as pedido;
 use \clases\Carrito as carrito;
-$c = new carrito( $_SESSION['rolUsusario']);
-$p = new pedido( $_SESSION['rolUsusario']);
+
+$c = new carrito($_SESSION['rolUsusario']);
+$p = new pedido($_SESSION['rolUsusario']);
 
 $id_usuario = $_SESSION['usuario'];
 $carrito = $_SESSION['carrito'];
@@ -27,23 +28,80 @@ $pedido = $p->crearPedido($id_usuario, $carrito, $precio, $cif, $modo_pago);
 // Si se creó el pedido, crea un array con el contenido del carrito, el precio total y las especificaciones adicionales,
 // crea el cuerpo del correo y lo envía al correo del usuario.
 // Si no se creó el pedido, muestra un mensaje de error.
-if($pedido) {
-$array_carrito = $p->array_carrito($carrito, $precio, $especif);
-$cuerpo = $p->crear_correo($array_carrito, $pedido);
-$p->enviar($_SESSION['mail'], $cuerpo);
-echo '<div class="layered box row mr-2"><h2 class="col-10 d-flex justify-content-center">Pedido realizado con éxito, te hemos enviado un correo.</h2></div>';
-$_SESSION['carrito'] = [];
-$c->add($id_usuario, NULL);
-} else {
-echo '<div class="layered box row mr-2"><h2 class="col-10 d-flex justify-content-center">Ocurrió algún error y no se pudo realizar el pedido</h2></div>';
-}
 ?>
-</br>
-<div class="layered box row mr-2">
+<div class="container main p-0 mt-5 card">
+    <?php
+    if ($pedido) {
+        $array_carrito = $p->array_carrito($carrito, $precio, $especif);
+        $cuerpo = $p->crear_correo($array_carrito, $pedido);
+        $p->enviar($_SESSION['mail'], $cuerpo);
+        echo ''
+        . '<div class="card-header text-center"><h2>Pedido confirmado.</h2></div>'
+        . '<div class="card-body">¡Muchas gracias por comprar con nosotros! <br>'
+        . 'Nos complace haber tenido la oportunidad de servirle y esperamos que haya disfrutado de su experiencia de compra. <br>'
+        . 'En nuestra empresa, nos esforzamos por ofrecer productos y servicios de alta calidad, y su satisfacción es nuestra máxima prioridad. <br>'
+        . 'Si tiene alguna pregunta o comentario sobre su compra, no dude en ponerse en contacto con nosotros. <br><br>'
+        . '¡Gracias de nuevo por elegirnos como su proveedor y esperamos poder servirle nuevamente en el futuro!</div>'
+        . '<div class="col-2 d-flex justify-content-right"><button type="button" class="btn btn-outline-secondary" id="download-button">Descargar factura en PDF</button></div>'
+        . '<div id="descargar">';
+         echo $cuerpo;
+        echo '</div>
+
+        <script>
+            const guardarpdf = document.getElementById("download-button");
+            function generarpdf() {
+                const element = document.getElementById("descargar");
+                html2pdf().from(element).save();
+            }
+
+            guardarpdf.addEventListener("click", generarpdf);
+        </script> ';;
+
+        echo '<div id="mensaje" class="rounded" style="z-index: 4; position: absolute; transition: top 0.5s; top: -150%; right: 0; background-color: #80ff00; color: black; padding: 10px;">';
+        echo '<h2>Pedido realizado con éxito, te hemos enviado un correo.</h2>';
+        echo '</div>';
+        echo "<script defer>
+              window.onload = function() {
+              var mensajeDiv = document.getElementById('mensaje');
+              mensajeDiv.style.top = '20%';
+              setTimeout(function() {
+                    mensajeDiv.style.top = '-150%';
+                    }, 5000);
+                    }
+              </script>";
+        $_SESSION['carrito'] = [];
+        $c->add($id_usuario, NULL);
+    } else {
+        //echo '<div class="layered box row mr-2"><h2 class="col-10 d-flex justify-content-center">Ocurrió algún error y no se pudo realizar el pedido</h2></div>';
+        echo ''
+        . '<div class="card-header text-center"><h2>Pedido confirmado.</h2></div>'
+        . '<div class="card-body">¡Muchas gracias por comprar con nosotros! <br>'
+        . 'Lamentamos sinceramente el error que ocurrió durante su intento de compra con nosotros. <br>'
+        . 'Por favor, acepte nuestras disculpas por cualquier inconveniente que esto haya causado. <br>'
+        . 'Si hay algo más que podamos hacer para ayudar a solucionar el problema, no dude en hacérnoslo saber. <br><br>'
+        . 'Gracias por su comprensión y por darnos la oportunidad de servirle mejor en el futuro.</div>';
+        
+        echo '<div id="mensaje" class="rounded" style="z-index: 4; position: absolute; transition: top 0.5s; top: -150%; right: 0; background-color: #f44336; color: white; padding: 10px;">';
+        echo '<h2>Ocurrió algún error y no se pudo realizar el pedido.</h2>';
+        echo '</div>';
+        echo "<script defer>
+              window.onload = function() {
+              var mensajeDiv = document.getElementById('mensaje');
+              mensajeDiv.style.top = '20%';
+              setTimeout(function() {
+                    mensajeDiv.style.top = '-150%';
+                    }, 5000);
+                    }
+              </script>";
+    }
+    ?>
+    <br>
+    <div class="card-footer">
         <div class="col-2 d-flex justify-content-right">
-        <a href="../login/indexLogin.php"><button type="button" class="btn btn-outline-success">Volver al inicio</button></a>
+            <a href="../login/indexLogin.php"><button type="button" class="btn btn-outline-secondary">Volver al inicio</button></a>
         </div>
-      </div>
+    </div>
+</div> <!--NO BORRAR ES EL FINAL DEL DIV DE ARRIBA-->
 
 <?php
-require(dirname(__DIR__,2) .DIRECTORY_SEPARATOR ."frontend". DIRECTORY_SEPARATOR . "php". DIRECTORY_SEPARATOR . "footer.php"); 
+require(dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . "frontend" . DIRECTORY_SEPARATOR . "php" . DIRECTORY_SEPARATOR . "footer.php");
--- frontend/php/nav.php
+++ frontend/php/nav.php
@@ -1,4 +1,5 @@
 <?php
+//ob_start();
 include dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . "autoloadClasesLogin.php";
 
 //echo"../../autoloadClasesLogin.php";
@@ -17,6 +18,8 @@ if (isset($_GET['red'])) {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
               integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
@@ -24,11 +27,17 @@ if (isset($_GET['red'])) {
                 integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
         crossorigin="anonymous"></script>
         <script src="https://kit.fontawesome.com/bd61d050b0.js" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/css/intlTelInput.css">
+        <script src="https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/intlTelInput.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"
+        integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 
         <link rel="stylesheet" href='<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "frontend" . DIRECTORY_SEPARATOR . "css" . DIRECTORY_SEPARATOR . "estilos.css"; ?>'/>
-
+        <link rel="stylesheet" href='<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "frontend" . DIRECTORY_SEPARATOR . "css" . DIRECTORY_SEPARATOR . "index.css"; ?>'/>
+        <script src="<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "frontend" . DIRECTORY_SEPARATOR . "js" . DIRECTORY_SEPARATOR . "main.js"; ?>" defer></script>
     </head>
 
     <body>
@@ -42,10 +51,10 @@ if (isset($_GET['red'])) {
                 <ul class="nav nav-tabs justify-content-end ">
 
                     <li class="nav-item"> 
-                        <a class="nav-link active" aria-current="page" href= '<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "index.php"; ?>' ><i class="fa-solid fa-house fa-xl pe-3 menu-responsive my-auto pe-2"></i>Inicio</a>
+                        <a class="nav-link active" aria-current="page" href= '<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "index.php"; ?>' ><i class="fa-solid fa-house fa-xl pe-4 menu-responsive my-auto"></i>Inicio</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false"><i class="fa-solid fa-utensils fa-2xl menu-responsive my-auto pe-2"></i>Carta</a>
+                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false"><i class="fa-solid fa-utensils fa-2xl menu-responsive my-auto pe-4"></i>Carta</a>
                         <ul class="dropdown-menu ">   
                             <?php
                             foreach ($categorias as $nombre11) {
@@ -60,38 +69,33 @@ if (isset($_GET['red'])) {
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href='<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "frontend" . DIRECTORY_SEPARATOR . "php" . DIRECTORY_SEPARATOR . "contacto.php"; ?>'><i class="fa-solid fa-file-signature fa-2xl menu-responsive my-auto pe-2"></i>Contacto</a>
+                        <a class="nav-link" href='<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "frontend" . DIRECTORY_SEPARATOR . "php" . DIRECTORY_SEPARATOR . "contacto.php"; ?>'><i class="fa-solid fa-file-signature fa-2xl menu-responsive my-auto pe-4"></i>Contacto</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href='<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "backend" . DIRECTORY_SEPARATOR . "usuario" . DIRECTORY_SEPARATOR . "reservas.php"; ?>'><i class="fa-solid fa-book fa-2xl menu-responsive my-auto pe-2"></i>Reservas</a>
+                        <a class="nav-link" href='<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "backend" . DIRECTORY_SEPARATOR . "usuario" . DIRECTORY_SEPARATOR . "reservas.php"; ?>'><i class="fa-solid fa-book fa-2xl menu-responsive my-auto pe-4"></i>Reservas</a>
                     </li>
                     <li class="nav-item">  
-                        <a class="nav-link" href='<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "backend" . DIRECTORY_SEPARATOR . "cart" . DIRECTORY_SEPARATOR . "index_carrito.php"; ?>'><i class="fa-solid fa-basket-shopping menu-responsive fa-2xl my-auto pe-2"></i>Cesta</a>
+                        <a class="nav-link" href='<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "backend" . DIRECTORY_SEPARATOR . "cart" . DIRECTORY_SEPARATOR . "index_carrito.php"; ?>'><i class="fa-solid fa-basket-shopping fa-2xl menu-responsive  my-auto pe-4"></i>Cesta</a>
                     </li>
+                    <li class="nav-item"> <button class="btn btn-link nav-link" onclick="changeTheme();"><i class="fa-solid fa-circle-half-stroke fa-xl pe-4 menu-responsive my-auto"></i>Tema</button> </li>
                     <?php
                     if (!empty($_SESSION['usuario']) && isset($_SESSION)) {
                         ?> 
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">Usuario</a>
+                            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false"><i class="fa-solid fa-user fa-2xl  menu-responsive my-auto pe-4"></i>Usuario</a>
                             <ul class="dropdown-menu ">   
-                                <li class="nav-item">
-                                    <a class="nav-link" href='<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "backend" . DIRECTORY_SEPARATOR . "usuario" . DIRECTORY_SEPARATOR . "modificarDatosUsuario.php"; ?>'>Cambiar Perfil</a>
-                                </li> 
+                                <li><a class="dropdown-item" href='<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "backend" . DIRECTORY_SEPARATOR . "usuario" . DIRECTORY_SEPARATOR . "modificarDatosUsuario.php"; ?>'>Cambiar Perfil</a>                                </li> 
                                 <li><a class="dropdown-item" href='<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "backend" . DIRECTORY_SEPARATOR . "usuario" . DIRECTORY_SEPARATOR . "index_pedidos.php"; ?>'>Pedidos</a></li>
                                 <li><a class="dropdown-item" href='<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "backend" . DIRECTORY_SEPARATOR . "usuario" . DIRECTORY_SEPARATOR . "verCambios.php"; ?>'>Histórico Datos</a></li>
                                 <li><hr class="dropdown-divider"></li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href='<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "backend" . DIRECTORY_SEPARATOR . "login" . DIRECTORY_SEPARATOR . "logout.php"; ?>'>Cerrar Session</a>
-                                </li>
+                                <li><a class="dropdown-item" href='<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "backend" . DIRECTORY_SEPARATOR . "login" . DIRECTORY_SEPARATOR . "logout.php"; ?>'>Cerrar Session</a></li>
                             </ul>
                         </li>
-
-
                         <?php
                     } elseif (!empty($_SESSION['administrador'])) {
                         ?> 
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false"><i class="fa-solid fa-user fa-2xl menu-responsive my-auto pe-2"></i>Administración</a>
+                            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false"><i class="fa-solid fa-user fa-2xl menu-responsive my-auto pe-4"></i>Administración</a>
                             <ul class="dropdown-menu ">
                                 <li>
                                     <a class="dropdown-item" href='<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "backend" . DIRECTORY_SEPARATOR . "administrador" . DIRECTORY_SEPARATOR . "indexAdministrador.php"; ?>'>Panel Administrador </a>
@@ -106,19 +110,14 @@ if (isset($_GET['red'])) {
                     } else {
                         ?> 
                         <li class="nav-item">
-                            <a class="nav-link" href='<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "backend" . DIRECTORY_SEPARATOR . "login" . DIRECTORY_SEPARATOR . "indexLogin.php"; ?>'>Login</a>
+                            <a class="nav-link" href='<?php echo DIRECTORY_SEPARATOR . "proyecto" . DIRECTORY_SEPARATOR . "backend" . DIRECTORY_SEPARATOR . "login" . DIRECTORY_SEPARATOR . "indexLogin.php"; ?>'><i class="fa-solid fa-right-to-bracket fa-2xl pe-4 menu-responsive my-auto"></i>Login</a>
                         </li>
                         <?php
                     }
                     ?>
                 </ul> 
             </div>
-            
-            <script>
-                const nav = document.getElementById('navbarNavAltMarkup');
-                console.log(document.getElementById('navbarNavAltMarkup'));
-                nav.style.top = nav.style.top == '0px' ? '-60vh' : '0px';
-            </script>
+
         </div>
 
 

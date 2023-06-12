ddf7918d42dc7e49a91c7d3387b864d29117ab12
a20luisgag <guille1insua@gmail.com>
2023-06-12 18:15:50 +0200

    sin cambios nuria

--- backend/clases/Pedido.php
+++ backend/clases/Pedido.php
@@ -10,7 +10,6 @@ namespace clases;
 use \clases\Mails as mails;
 use \PDO;
 use \PDOException;
-use \clases\CrearPDF;
 
 class Pedido extends Conexion {
 
@@ -228,14 +227,8 @@ class Pedido extends Conexion {
         $texto .= $carrito;
         $texto .= "<tr><td colspan=3> Modo de pago: " . $result["nombre"] . "</td></tr>";
         $texto .= "<tr><td colspan=3> Su pedido se está cocinando... </td></tr></table>";
-        
-        $this->crearpdf($texto);
         return $texto;
     }
-
-    function crearpdf($texto) {
-        new CrearPDF($texto);
-    } 
 
     /**
     * Envía el correo electrónico con el contenido proporcionado.
